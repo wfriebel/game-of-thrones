9ee647156ac9df48ef464428a1c65bd72a3de97c
Will Friebel <will.friebel@gmail.com>
2017-07-23 22:07:50 -0700

    Declare ruby version

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170721070955) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "characters", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
